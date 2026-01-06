4119b0dc1a4a39e4bc3a0d69909e3218bc0e8ae9
yousif <yousif.sei07@gmail.com>
2026-01-06 19:08:13 +0300

    Securiy Feature yousif

---
+++ src/main/java/com/ga/airticketmanagement/model/request/LoginRequest.java
@@ -0,0 +1,10 @@
+package com.ga.airticketmanagement.model.request;
+
+import lombok.Getter;
+
+@Getter
+public class LoginRequest {
+    private String email;
+    private String password;
+
+}
---
+++ src/main/java/com/ga/airticketmanagement/security/MyUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.ga.airticketmanagement.security;
+
+import com.ga.airticketmanagement.model.User;
+import com.ga.airticketmanagement.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class MyUserDetailsService implements UserDetailsService {
+
+    private UserService userService;
+
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+    @Override
+
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+         User user = userService.findUserByEmailAddress(email);
+         return new MyUserDetails(user);
+    }
+}
