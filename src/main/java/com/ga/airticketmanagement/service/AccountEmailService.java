0322efe2c46bb2f7aaaec3793d6d4bae8810eea8
Husain Habib <habib.h.e@outlook.com>
2026-01-07 17:35:39 +0300

    implement AccountEmailService for clear business logic (email verification, password reset) emails

--- src/main/java/com/ga/airticketmanagement/service/AccountEmailService.java
+++ src/main/java/com/ga/airticketmanagement/service/AccountEmailService.java
@@ -1,4 +1,26 @@
 package com.ga.airticketmanagement.service;
 
+import com.ga.airticketmanagement.model.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AccountEmailService {
+
+    @Value("${app.frontend.base-url}")
+    private String baseUrl;
+    private final EmailService emailService;
+
+    public AccountEmailService(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    public void sendVerificationEmail(User user, String token) {
+        emailService.sendHtmlEmail(
+                user.getEmailAddress(),
+                "Verify your email",
+                "Please verify your email address to gain access to your account." +
+                        "<br><br><a href=\""+baseUrl+"/auth/users/verify?token="+ token +"\">Verify Email</a>"
+        );
+    }
 }
