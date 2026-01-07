e0efe7cb927505ea3187f9e08114073be72e3dc8
Husain Habib <habib.h.e@outlook.com>
2026-01-07 15:59:09 +0300

    implement token generator

---
+++ src/main/java/com/ga/airticketmanagement/util/TokenGenerator.java
@@ -0,0 +1,16 @@
+package com.ga.airticketmanagement.util;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class TokenGenerator {
+
+    private static final SecureRandom secureRandom = new SecureRandom();
+    private static final Base64.Encoder encoder = Base64.getUrlEncoder().withoutPadding();
+
+    public static String generateToken() {
+        byte[] randomBytes = new byte[32];
+        secureRandom.nextBytes(randomBytes);
+        return encoder.encodeToString(randomBytes);
+    }
+}
