d5e470d63249574a2e3296d5984877856bb10967
Husain Habib <habib.h.e@outlook.com>
2026-01-07 17:32:42 +0300

    update to implementation to include simple text and html email sending capability

--- src/main/java/com/ga/airticketmanagement/service/SmtpEmailService.java
+++ src/main/java/com/ga/airticketmanagement/service/SmtpEmailService.java
@@ -1,8 +1,15 @@
 package com.ga.airticketmanagement.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.http.HttpStatus;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+@Service
 public class SmtpEmailService implements EmailService {
 
     private final JavaMailSender mailSender;
@@ -12,11 +19,26 @@ public class SmtpEmailService implements EmailService {
     }
 
     @Override
-    public void sendEmail(String to, String subject, String body) {
+    public void sendTxtEmail(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
         message.setSubject(subject);
         message.setText(body);
         mailSender.send(message);
+    }
+
+    @Override
+    public void sendHtmlEmail(String to, String subject, String body) {
+        try{
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+            mailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            throw new RuntimeException("Failed to send email", e);
+        }
+
     }
 }
