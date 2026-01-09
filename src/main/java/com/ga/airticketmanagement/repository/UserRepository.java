46cb3cb0a8df8ccdb5b7b98203e846ad747246cc
Husain Habib <habib.h.e@outlook.com>
2026-01-09 04:09:34 +0300

    update return type to Optional<User>

--- src/main/java/com/ga/airticketmanagement/repository/UserRepository.java
+++ src/main/java/com/ga/airticketmanagement/repository/UserRepository.java
@@ -4,9 +4,11 @@ import com.ga.airticketmanagement.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmailAddress(String userEmailAddress);
-    User findUserByEmailAddress(String userEmailAddress);
+    Optional<User> findUserByEmailAddress(String userEmailAddress);
 
 }
