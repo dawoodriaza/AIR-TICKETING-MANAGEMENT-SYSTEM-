4c6bc7b80489c28e10bfdc0217c44447348620bb
Husain Habib <habib.h.e@outlook.com>
2026-01-11 16:33:33 +0300

    update flightRepo methods to accept Pageable

--- src/main/java/com/ga/airticketmanagement/repository/FlightRepository.java
+++ src/main/java/com/ga/airticketmanagement/repository/FlightRepository.java
@@ -1,6 +1,8 @@
 package com.ga.airticketmanagement.repository;
 
 import com.ga.airticketmanagement.model.Flight;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,8 +18,10 @@ public interface FlightRepository extends JpaRepository<Flight, Long> {
     Optional<Flight> findByIdAndAirport(@Param("airportId") Long airportId, @Param("flightId") Long flightId);
     @Query("SELECT f FROM Flight f WHERE f.originAirport.id = :airportId OR f.destinationAirport.id = :airportId")
     List<Flight> findByAirport(@Param("airportId") Long airportId);
+    @Query("SELECT f FROM Flight f WHERE f.originAirport.id = :airportId OR f.destinationAirport.id = :airportId")
+    Page<Flight> findByAirport(@Param("airportId") Long airportId, Pageable pageable);
     @Query("SELECT f FROM Flight f WHERE f.originAirport.id = :airportId")
-    List<Flight> findByOriginAirport(@Param("airportId") Long airportId);
+    Page<Flight> findByOriginAirport(@Param("airportId") Long airportId, Pageable pageable);
     @Query("SELECT f FROM Flight f WHERE f.destinationAirport.id = :airportId")
-    List<Flight> findByDestinationAirport(@Param("airportId") Long airportId);
+    Page<Flight> findByDestinationAirport(@Param("airportId") Long airportId, Pageable pageable);
 }
