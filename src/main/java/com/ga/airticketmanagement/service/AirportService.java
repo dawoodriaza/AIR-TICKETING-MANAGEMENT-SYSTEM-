315612d5e389557a290d2f0aad51616de3ec8395
Husain Habib <habib.h.e@outlook.com>
2026-01-11 11:37:54 +0300

    update to use DTOs and incorporate pagination for list responses

--- src/main/java/com/ga/airticketmanagement/service/AirportService.java
+++ src/main/java/com/ga/airticketmanagement/service/AirportService.java
@@ -1,77 +1,106 @@
 package com.ga.airticketmanagement.service;
 
+import com.ga.airticketmanagement.dto.mapper.AirportMapper;
+import com.ga.airticketmanagement.dto.mapper.PageMetaFactory;
+import com.ga.airticketmanagement.dto.request.AirportRequest;
+import com.ga.airticketmanagement.dto.response.ListResponse;
+import com.ga.airticketmanagement.dto.response.PageMeta;
+import com.ga.airticketmanagement.dto.response.AirportResponse;
+import com.ga.airticketmanagement.exception.InformationFoundException;
+import com.ga.airticketmanagement.exception.InformationNotFoundException;
 import com.ga.airticketmanagement.model.Airport;
+import com.ga.airticketmanagement.model.User;
 import com.ga.airticketmanagement.repository.AirportRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.ga.airticketmanagement.repository.UserRepository;
+import com.ga.airticketmanagement.security.AuthenticatedUserProvider;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class AirportService {
 
+    private final AirportMapper airportMapper;
     private AirportRepository airportRepository;
-
-    @Autowired
-    public void setAirportRepository(AirportRepository airportRepository){
+    private AuthenticatedUserProvider authenticatedUserProvider;
+    public AirportService(AirportMapper airportMapper, AirportRepository airportRepository,
+                          AuthenticatedUserProvider authenticatedUserProvider) {
+        this.airportMapper = airportMapper;
         this.airportRepository = airportRepository;
+        this.authenticatedUserProvider = authenticatedUserProvider;
     }
 
-    public Airport createAirport(Airport airportObject){
+    public AirportResponse createAirport(AirportRequest airportObject){
 
-        airportRepository.findByName(airportObject.getName()).ifPresent(
-                a -> new ResponseStatusException(HttpStatus.CONFLICT, "Airport " + a.getName() + " already exists.")
-        );
+        User user = authenticatedUserProvider.getAuthenticatedUser();
 
-        return airportRepository.save(airportObject);
+        Airport airport = airportMapper.toEntity(airportObject);
+        airportRepository.findByName(airportObject.getName())
+                .ifPresent(
+                a -> {
+                    throw new InformationFoundException("Airport with name " + airportObject.getName() + " already exists");
+                });
+
+        airport.setUser(user);
+        airport = airportRepository.save(airport);
+        return airportMapper.toResponse(airport);
     }
 
-    public Airport getAirport(Long id){
+    public AirportResponse getAirport(Long id){
 
         Airport airport = airportRepository.findById(id).orElseThrow(
-                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Airport " + id + " does not exist.")
+                () -> new InformationNotFoundException("Airport with id " + id + " not found")
         );
 
-        return  airport;
+        return  airportMapper.toResponse(airport);
     }
 
 
-    public List<Airport> getAirports(){
+    public ListResponse<AirportResponse> getAirports(Pageable pageable) {
 
-        List<Airport> airports = airportRepository.findAll();
+        Page<Airport> page = airportRepository.findAll(pageable);
 
-        return airports;
+        List<AirportResponse> data = page.getContent().stream()
+                .map(airportMapper::toResponse).toList();
+
+        PageMeta meta = PageMetaFactory.from(page);
+
+        return new ListResponse<>(data, meta);
     }
 
-    public Airport updateAirport(Long id, Airport airportObject){
+    public AirportResponse updateAirport(Long id, AirportRequest airportObject){
 
         Airport airport = airportRepository.findById(id).orElseThrow(
-                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Airport " + id + " does not exist.")
+                () -> new InformationNotFoundException("Airport " + id + " does not exist.")
         );
 
-        Optional<Airport> exists = airportRepository.findByName(airportObject.getName());
-
-        if(exists.isPresent() && exists.get().getName().equals(airportObject.getName())){
-            throw new ResponseStatusException(HttpStatus.CONFLICT, "Airport " + airportObject.getName() + " already exists.");
-        }
+       airportRepository.findByName(airportObject.getName()).ifPresent(
+            existAirport -> {
+                if (!existAirport.getId().equals(id)) {
+                    throw new InformationFoundException("Airport with name " + airportObject.getName() + " already exists");
+                }
+            });
 
         airport.setName(airportObject.getName());
         airport.setCountry(airportObject.getCountry());
         airport.setCode(airportObject.getCode());
+        Airport updatedAirport = airportRepository.save(airport);
 
-        return airportRepository.save(airport);
+        return airportMapper.toResponse(updatedAirport);
     }
 
-    public String deleteAirport(Long id){
+
+    public void deleteAirport(Long id){
 
         Airport airport = airportRepository.findById(id).orElseThrow(
-                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Airport " + id + " does not exist.")
+                () -> new InformationNotFoundException("Airport " + id + " does not exist")
         );
 
         airportRepository.delete(airport);
-
-        return "Airport " + id + " was deleted.";
     }
 }
