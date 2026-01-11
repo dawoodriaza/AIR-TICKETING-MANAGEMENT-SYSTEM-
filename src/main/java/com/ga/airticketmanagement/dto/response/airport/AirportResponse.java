aa4af20fef5b7cc6177b8228d495c171c401916a
Husain Habib <habib.h.e@outlook.com>
2026-01-11 11:12:17 +0300

    remove left over files after previous rename

--- src/main/java/com/ga/airticketmanagement/dto/response/airport/AirportResponse.java
+++
@@ -1,8 +0,0 @@
-package com.ga.airticketmanagement.dto.response.airport;
-
-public record CreateAirportResponse (
-        Long id,
-        String name,
-        String country,
-        String code
-){}
