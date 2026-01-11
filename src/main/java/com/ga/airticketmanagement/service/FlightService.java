598de88a69b5aefd37d5cfa8eed1d068a216adc9
Husain Habib <habib.h.e@outlook.com>
2026-01-11 16:35:05 +0300

    update flight service methods to return Flight DTOs, utilize pagination for list responses, throw proper exceptions

--- src/main/java/com/ga/airticketmanagement/service/FlightService.java
+++ src/main/java/com/ga/airticketmanagement/service/FlightService.java
@@ -1,157 +1,203 @@
 package com.ga.airticketmanagement.service;
 
-import com.ga.airticketmanagement.dto.request.CreateFlightByOriginAirportRequest;
-import com.ga.airticketmanagement.dto.request.CreateFlightRequest;
+import com.ga.airticketmanagement.dto.mapper.FlightMapper;
+import com.ga.airticketmanagement.dto.mapper.PageMetaFactory;
+import com.ga.airticketmanagement.dto.request.FlightRequest;
 import com.ga.airticketmanagement.dto.request.UpdateFlightByOriginAirportRequest;
 import com.ga.airticketmanagement.dto.request.UpdateFlightRequest;
+import com.ga.airticketmanagement.dto.response.FlightResponse;
+import com.ga.airticketmanagement.dto.response.ListResponse;
+import com.ga.airticketmanagement.dto.response.PageMeta;
+import com.ga.airticketmanagement.exception.ConflictedOriginDestinationException;
+import com.ga.airticketmanagement.exception.InformationNotFoundException;
 import com.ga.airticketmanagement.model.Airport;
 import com.ga.airticketmanagement.model.Flight;
+import com.ga.airticketmanagement.model.User;
 import com.ga.airticketmanagement.repository.AirportRepository;
 import com.ga.airticketmanagement.repository.FlightRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import com.ga.airticketmanagement.security.AuthenticatedUserProvider;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class FlightService {
 
-    private FlightRepository flightRepository;
-    private AirportRepository airportRepository;
-
-
-    @Autowired
-    public void setFlightRepository(FlightRepository flightRepository) {
+    private final FlightRepository flightRepository;
+    private final AirportRepository airportRepository;
+    private final FlightMapper flightMapper;
+    private final AuthenticatedUserProvider authenticatedUserProvider;
+    public FlightService(FlightRepository flightRepository, AirportRepository airportRepository
+            , FlightMapper flightMapper, AuthenticatedUserProvider authenticatedUserProvider) {
         this.flightRepository = flightRepository;
-    }
-
-    @Autowired
-    public void setAirportRepository(AirportRepository airportRepository) {
         this.airportRepository = airportRepository;
+        this.flightMapper = flightMapper;
+        this.authenticatedUserProvider = authenticatedUserProvider;
     }
 
+    public FlightResponse createFlightByOriginAirport(Long originAirportId, FlightRequest flightObject) {
 
-    public Flight createFlightByOriginAirport(Long originAirportId, CreateFlightByOriginAirportRequest flightObject) {
-        System.out.println(flightObject);
-        Airport origin = airportRepository.findById(originAirportId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Airport not found"));
-        Airport destination = airportRepository.findById(flightObject.getDestinationAirportId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Destination Airport not found"));
+        Airport origin = airportRepository.findById(originAirportId).orElseThrow(
+                () -> new InformationNotFoundException("Airport not found"));
+        Airport destination = airportRepository.findById(flightObject.getDestinationAirportId()).orElseThrow(
+                () -> new InformationNotFoundException("Destination Airport not found"));
+        User user = authenticatedUserProvider.getAuthenticatedUser();
 
         if(origin.getId().equals(destination.getId())){
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Destination Airport and Destination Airport are the same");
+            throw new ConflictedOriginDestinationException("Destination Airport and Destination Airport are the same");
         }
 
-        Flight flight = new Flight();
+        Flight flight = flightMapper.toEntity(flightObject);
         flight.setOriginAirport(origin);
         flight.setDestinationAirport(destination);
         flight.setPrice(flightObject.getPrice());
         flight.setDepartureTime(flightObject.getDepartureTime());
         flight.setArrivalTime(flightObject.getArrivalTime());
+        flight.setUser(user);
 
-        return flightRepository.save(flight);
+        flight = flightRepository.save(flight);
+        return flightMapper.toResponse(flight);
     }
 
-    public Flight createFlight(CreateFlightRequest flightObject) {
+    public FlightResponse createFlight(FlightRequest flightObject) {
 
-        Airport origin = airportRepository.findById(flightObject.getOriginAirportId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Airport not found"));
-        Airport destination = airportRepository.findById(flightObject.getDestinationAirportId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Destination Airport not found"));
+        Airport origin = airportRepository.findById(flightObject.getOriginAirportId()).orElseThrow(
+                () -> new InformationNotFoundException("Airport not found"));
+        Airport destination = airportRepository.findById(flightObject.getDestinationAirportId()).orElseThrow(
+                () -> new InformationNotFoundException("Destination Airport not found"));
+        User user = authenticatedUserProvider.getAuthenticatedUser();
 
         if(origin.getId().equals(destination.getId())){
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Destination Airport and Destination Airport are the same");
+            throw new ConflictedOriginDestinationException("Destination Airport and Destination Airport are the same");
         }
 
-        Flight flight = new Flight();
+        Flight flight = flightMapper.toEntity(flightObject);
         flight.setOriginAirport(origin);
         flight.setDestinationAirport(destination);
         flight.setPrice(flightObject.getPrice());
         flight.setDepartureTime(flightObject.getDepartureTime());
         flight.setArrivalTime(flightObject.getArrivalTime());
+        flight.setUser(user);
 
-        return flightRepository.save(flight);
+        flight = flightRepository.save(flight);
+        return flightMapper.toResponse(flight);
     }
 
-    public Flight updateFlightByOriginAirport(Long airportId, Long flightId, UpdateFlightByOriginAirportRequest flightObject) {
+    public FlightResponse updateFlightByOriginAirport(Long airportId, Long flightId, UpdateFlightByOriginAirportRequest flightObject) {
 
         Flight flight = validateFlight(airportId, flightId);
-        Airport destinationAirport = airportRepository.findById(flightObject.getDestinationAirportId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Destination Airport not found"));
+        Airport destinationAirport = airportRepository.findById(flightObject.getDestinationAirportId()).orElseThrow(
+                () -> new InformationNotFoundException("Destination Airport not found"));
 
         flight.setDestinationAirport(destinationAirport);
         flight.setDepartureTime(flightObject.getDepartureTime());
         flight.setArrivalTime(flightObject.getArrivalTime());
         flight.setPrice(flightObject.getPrice());
+        flight =  flightRepository.save(flight);
 
-        return flightRepository.save(flight);
+        return flightMapper.toResponse(flight);
     }
 
-    public Flight updateFlight(Long flightId,UpdateFlightRequest flightObject) {
+    public FlightResponse updateFlight(Long flightId,UpdateFlightRequest flightObject) {
 
-        Flight flight = flightRepository.findById(flightId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Flight not found"));
-        Airport originAirport = airportRepository.findById(flightObject.getOriginAirportId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Destination Airport not found"));
-        Airport destinationAirport = airportRepository.findById(flightObject.getDestinationAirportId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Destination Airport not found"));
+        Flight flight = flightRepository.findById(flightId).orElseThrow(
+                () -> new InformationNotFoundException("Flight not found"));
+        Airport originAirport = airportRepository.findById(flightObject.getOriginAirportId()).orElseThrow(
+                () -> new InformationNotFoundException("Destination Airport not found"));
+        Airport destinationAirport = airportRepository.findById(flightObject.getDestinationAirportId()).orElseThrow(
+                () -> new InformationNotFoundException("Destination Airport not found"));
 
         flight.setOriginAirport(originAirport);
         flight.setDestinationAirport(destinationAirport);
         flight.setDepartureTime(flightObject.getDepartureTime());
         flight.setArrivalTime(flightObject.getArrivalTime());
         flight.setPrice(flightObject.getPrice());
-
-        return flightRepository.save(flight);
+        flight = flightRepository.save(flight);
+        return  flightMapper.toResponse(flight);
     }
 
-    public String deleteFlight(Long flightId) {
+    public void deleteFlight(Long flightId) {
 
-        Flight flight = flightRepository.findById(flightId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Flight not found"));
+        Flight flight = flightRepository.findById(flightId).orElseThrow(
+                () -> new InformationNotFoundException("Flight not found"));
 
         flightRepository.delete(flight);
-
-        return "Flight deleted successfully";
     }
 
-    public Flight getFlight(Long airportId, Long flightId) {
+    public FlightResponse getFlight(Long flightId) {
+
+        Flight flight = flightRepository.findById(flightId).orElseThrow(
+                () -> new InformationNotFoundException("Flight not found")
+        );
+
+        return flightMapper.toResponse(flight);
+    }
+
+    public FlightResponse getFlightOfAirport(Long airportId, Long flightId) {
 
         Flight flight = validateFlight(airportId, flightId);
 
-        return flight;
+        return flightMapper.toResponse(flight);
     }
 
-    public List<Flight> getFlights() {
+    public ListResponse<FlightResponse> getFlights(Pageable pageable) {
 
-        return flightRepository.findAll();
+        Page<Flight> page = flightRepository.findAll(pageable);
 
-    }
+        List<FlightResponse> data = page.stream().map(flightMapper::toResponse).toList();
+        PageMeta meta = PageMetaFactory.from(page);
 
-    public List<Flight> getAirportFlights(Long airportId) {
-
-        Airport airport = airportRepository.findById(airportId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Airport not found"));
-
-        return flightRepository.findByAirport(airport.getId());
+        return new ListResponse<>(data, meta);
 
     }
 
-    public List<Flight> getAirportDepartures(Long airportId) {
-        Airport airport = airportRepository.findById(airportId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Airport not found"));
+    public ListResponse<FlightResponse> getAirportFlights(Long airportId, Pageable pageable) {
 
-        return flightRepository.findByOriginAirport(airport.getId());
+        Airport airport = airportRepository.findById(airportId).orElseThrow(
+                () -> new InformationNotFoundException("Airport not found"));
+
+        Page<Flight> page = flightRepository.findByAirport(airport.getId(), pageable);
+        List<FlightResponse> data =  page.stream().map(flightMapper::toResponse).toList();
+        PageMeta meta = PageMetaFactory.from(page);
+
+        return new ListResponse<>(data, meta);
     }
 
-    public List<Flight> getAirportArrivals(Long airportId) {
-        Airport airport = airportRepository.findById(airportId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Airport not found"));
+    public ListResponse<FlightResponse> getAirportDepartures(Long airportId,  Pageable pageable) {
 
-        return flightRepository.findByDestinationAirport(airport.getId());
+        Airport airport = airportRepository.findById(airportId).orElseThrow(
+                () -> new InformationNotFoundException("Airport not found"));
+
+        Page<Flight> page = flightRepository.findByOriginAirport(airport.getId(), pageable);
+        List<FlightResponse> data =  page.stream().map(flightMapper::toResponse).toList();
+        PageMeta meta = PageMetaFactory.from(page);
+
+        return new ListResponse<>(data, meta);
+    }
+
+    public ListResponse<FlightResponse> getAirportArrivals(Long airportId, Pageable pageable) {
+
+        Airport airport = airportRepository.findById(airportId).orElseThrow(
+                () -> new InformationNotFoundException("Airport not found"));
+
+        Page<Flight> page = flightRepository.findByDestinationAirport(airport.getId(), pageable);
+        List<FlightResponse> data =  page.stream().map(flightMapper::toResponse).toList();
+        PageMeta meta = PageMetaFactory.from(page);
+
+        return  new ListResponse<>(data, meta);
     }
 
     private Flight validateFlight(Long airportId, Long flightId) {
-        airportRepository.findById(airportId).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Airport " + airportId + " not found"));
+
+        airportRepository.findById(airportId).orElseThrow(
+                ()-> new InformationNotFoundException("Airport " + airportId + " not found"));
 
         Flight flight = flightRepository.findByIdAndAirport(airportId, flightId).orElseThrow(
-                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Flight " + flightId + " not found")
+                () -> new InformationNotFoundException("Flight " + flightId + " not found")
         );
 
         return flight;
     }
-
-
 }
